d576bbf3ca1ce7fb514133aeaaf6334994531593
EthanLDot <ethanlee2337@gmail.com>
2021-09-18 11:13:02 -0700

    Revert "semi functional implementation"
    This reverts commit d52dbaf764281d25f20dd98e53e87e4aa1d35169.

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: "getCount"}, function(count) {
+        document.getElementById("myText").innerHTML = 'AAAAAAAAAAAAAAAAA';
+    });
+});
+
+
 document.addEventListener('DOMContentLoaded', function(){
     var changeFontButton = document.getElementById('changeFont');
     changeFontButton.addEventListener('click', function(){

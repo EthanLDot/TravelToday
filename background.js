302efb86bace0e67532f14b8a0b34eb8be1b0cec
EthanLDot <ethanlee2337@gmail.com>
2021-09-19 01:54:02 -0700

    Context menu fixed

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
+var obj;
+fetch('countryName2.json')
+    .then(response => response.json())
+    .then(data => {
+            obj = data;
+            //console.log(myData);
+            chrome.contextMenus.onClicked.addListener(function (params, tab) {
+            for (var i = 0; i < obj.length; i++){
+                if (obj[i].name == params.selectionText){
+                    chrome.tabs.create({url: 'https://www.kayak.com/travel-restrictions?&origin=' + obj[i].countryCode});
+                }
+            }
+        });
+        }
+    )
+    .catch(error => console.log(error));
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "open_new_tab") {
@@ -5,3 +22,10 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+chrome.contextMenus.create({
+    id: "",
+    title: 'Using TravelToday searching %s',
+    contexts: ['selection'],
+});
+
+
